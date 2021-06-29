2f9d37a9ade47f779ca7a8e6a32683742a481e15
alexelias <alex.elias@finmatch.com.br>
2021-06-28 21:34:32 -0300

    sistema de rotas

---
+++ Frontend/cidadera/src/routes.tsx
@@ -0,0 +1,55 @@
+import { Suspense, lazy } from 'react';
+import type { PartialRouteObject } from 'react-router';
+import AuthGuard from './components/AuthGuard';
+import Layout from './components/dashboard/Layout';
+import GuestGuard from './components/GuestGuard';
+
+
+const Loadable = (Component) => (props) => (
+  <Suspense fallback={<div></div>}>
+    <Component {...props} />
+  </Suspense>
+);
+
+const Login = Loadable(lazy(() => import('./pages/authentication/Login')));
+const Inicio = Loadable(lazy(() => import('./pages/dashboard/Inicio')));
+const Reclamacoes = Loadable(lazy(() => import('./pages/reclamacao/Reclamacoes')));
+
+const routes: PartialRouteObject[] = [
+  {
+    path: '/',
+    children: [
+      {
+        element: (
+          <GuestGuard>
+            <Login />
+          </GuestGuard>
+        )
+      },
+      {
+        path: 'login-unguarded',
+        element: <Login />
+      }
+    ]
+  },
+  {
+    path: 'dashboard',
+    element: (
+      <AuthGuard>
+        <Layout />
+      </AuthGuard>
+    ),
+    children: [
+      {
+        path: '/',
+        element: <Inicio />
+      },
+      {
+        path: '/reclamacoes',
+        element: <Reclamacoes />
+      },
+    ]
+  }
+];
+
+export default routes;
